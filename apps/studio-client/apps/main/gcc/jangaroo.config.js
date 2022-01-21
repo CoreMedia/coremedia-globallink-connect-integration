db4f4efb9d39544ead864dbe6d8850173e37443e
Alexander Holtkamp <alexander.holtkamp@coremedia.com>
2022-01-21 16:48:22 +0100

    fixed & cleaned up studio-client package.json / jangaroo.config.js
    - sencha entries for "name" and "namespace" are not necessary
    - applied proper naming pattern to extension packages
    - fixed config for jangaroo publish
    - added proper exports
    - eslint should be a caret version for easier upgrades

--- apps/studio-client/apps/main/gcc/jangaroo.config.js
+++ apps/studio-client/apps/main/gcc/jangaroo.config.js
@@ -2,8 +2,4 @@ const { jangarooConfig } = require("@jangaroo/core");
 
 module.exports = jangarooConfig({
   type: "code",
-  sencha: {
-    name: "com.coremedia.labs.translation.gcc__gcc-studio-client.main",
-    namespace: "com.coremedia.labs.translation.gcc.main",
-  },
 });
--- apps/studio-client/shared/ext/gcc/jangaroo.config.js
+++ apps/studio-client/shared/ext/gcc/jangaroo.config.js
@@ -2,10 +2,6 @@ const { jangarooConfig } = require("@jangaroo/core");
 
 module.exports = jangarooConfig({
   type: "code",
-  sencha: {
-    name: "com.coremedia.labs.translation.gcc__gcc-studio-client",
-    namespace: "com.coremedia.labs.translation.gcc",
-  },
   autoLoad: [
     "./src/GccWorkflowPlugin",
   ],
