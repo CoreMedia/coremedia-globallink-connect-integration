67b10a5391a7575ee4690c5c557b48794e0c7b23
Mark Michaelis <mark.michaelis@coremedia.com>
2025-01-21 09:53:37 +0100

    refactor(mock): Order Enum Entries by Name

--- apps/workflow-server/gcc-workflow-server-facade/gcc-restclient-facade-mock/src/main/java/com/coremedia/labs/translation/gcc/facade/mock/settings/MockError.java
+++ apps/workflow-server/gcc-workflow-server-facade/gcc-restclient-facade-mock/src/main/java/com/coremedia/labs/translation/gcc/facade/mock/settings/MockError.java
@@ -10,25 +10,26 @@ import java.util.Optional;
  */
 public enum MockError {
   /**
-   * Provokes an invalid XLIFF, that cannot be imported.
-   */
-  DOWNLOAD_XLIFF,
-  /**
-   * Provokes a communication failure during the download.
-   */
-  DOWNLOAD_COMMUNICATION,
-  /**
-   * Provokes a communication failure during the upload.
-   */
-  UPLOAD_COMMUNICATION,
-  /**
    * Provokes a communication failure during the cancellation request.
    */
   CANCEL_COMMUNICATION,
   /**
    * Provokes a server-side failure during the cancellation request.
    */
-  CANCEL_RESULT;
+  CANCEL_RESULT,
+  /**
+   * Provokes a communication failure during the download.
+   */
+  DOWNLOAD_COMMUNICATION,
+  /**
+   * Provokes an invalid XLIFF, that cannot be imported.
+   */
+  DOWNLOAD_XLIFF,
+  /**
+   * Provokes a communication failure during the upload.
+   */
+  UPLOAD_COMMUNICATION,
+  ;
 
   @NonNull
   public static Optional<MockError> tryParse(@NonNull String value) {
