8c919c7477567bd73d215e6bf3b174797a9c3d6f
Mark Michaelis <mark.michaelis@coremedia.com>
2025-01-27 13:12:49 +0100

    refactor(gcc-facade): Remove Obfuscation
    The only relevant authorization item that should not appear is the
    API-Key. A connector key is no real secret. Also, it should be the
    task of the `GCFacade` to hide secrets, if any, not ours.
    
    Removing this extra effort.

--- apps/workflow-server/gcc-workflow-server-facade/gcc-restclient-facade-default/src/main/java/com/coremedia/labs/translation/gcc/facade/def/SLF4JHandler.java
+++ apps/workflow-server/gcc-workflow-server-facade/gcc-restclient-facade-default/src/main/java/com/coremedia/labs/translation/gcc/facade/def/SLF4JHandler.java
@@ -7,14 +7,12 @@ import org.slf4j.LoggerFactory;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
-import java.util.regex.Pattern;
 
 /**
  * Handler for JUL Logger required for
  * {@link org.gs4tr.gcc.restclient.GCConfig#setLogger(java.util.logging.Logger)}.
  */
 final class SLF4JHandler extends Handler {
-  private static final Pattern SENSITIVE_KEY_PATTERN = Pattern.compile("([kK]ey)=(.{0,3}).*\\b");
   @NonNull
   private final Logger slf4jLogger;
 
@@ -41,7 +39,7 @@ final class SLF4JHandler extends Handler {
   @Override
   public void publish(@NonNull LogRecord record) {
     Level level = record.getLevel();
-    String message = obfuscateMessage(record.getMessage());
+    String message = record.getMessage();
     Throwable thrown = record.getThrown();
 
     if (level == Level.SEVERE) {
@@ -55,18 +53,6 @@ final class SLF4JHandler extends Handler {
     } else {
       slf4jLogger.trace(message, thrown);
     }
-  }
-
-  /**
-   * Replace sensitive data with asterisks.
-   *
-   * @param message the message to obfuscate
-   * @return the obfuscated message
-   */
-  @NonNull
-  public static String obfuscateMessage(@NonNull String message) {
-    message = SENSITIVE_KEY_PATTERN.matcher(message).replaceAll("$1=$2***");
-    return message;
   }
 
   @Override
