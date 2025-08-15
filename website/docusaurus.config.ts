cd5c70a1868e6fb817d5940f12f6dfcd63036afb
Mark Michaelis <mark.michaelis@coremedia.com>
2025-08-15 18:33:54 +0200

    docs: Update Edit Link
    Still pointing to Docusaurus. Changing to the location this
    documentation will have on main branch.

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -2,7 +2,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import type {Options as DocsOptions} from '@docusaurus/plugin-content-docs';
-import * as path from 'path';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -109,7 +108,7 @@ const config: Config = {
           routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: './sidebars.ts',
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/CoreMedia/coremedia-globallink-connect-integration/tree/main/website/',
           // remark-file-list: We need to add this before the default plugins,
           // so that we benefit from dynamic asset links being created.
           beforeDefaultRemarkPlugins: [
